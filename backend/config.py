db0d6a93f241d9c480040e2c18b7528e4f8ee4b8
Simon1_Shih <s7088953@gmail.com>
2026-06-02 11:43:37 +0800

    chore: 載入本機環境變數

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,13 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 INSTANCE_DIR = BASE_DIR / 'backend' / 'instance'
+
+load_dotenv(BASE_DIR / '.env.development.local')
+load_dotenv(BASE_DIR / '.env.local')
 
 
 def get_database_uri():
